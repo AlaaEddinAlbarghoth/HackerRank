c873f0ce1b12d0230842d20c0b8e97774746b55a
Alaa Eddin Albarghoth <eng.alaa.brghoth@gmail.com>
2022-08-01 23:10:52 +0300

    Initial Commit

---
+++ src/main/kotlin/hackerRank/algorithms/SolveMeFirst.kt
@@ -0,0 +1,14 @@
+package hackerRank.algorithms
+
+/*
+* Complete the function solveMeFirst to compute the sum of two integers.
+* Function Description
+    Complete the solveMeFirst function in the editor below.
+    solveMeFirst has the following parameters:
+    int a: the first value
+    int b: the second value
+    Returns
+    - int: the sum of a and b
+* */
+
+fun solveMeFirst(a: Int, b: Int): Int = a + b
